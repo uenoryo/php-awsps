4a76242f5745f296c7217973507764faf0e7c533
uenoryo <r.r.ag0611@outlook.jp>
2019-01-27 13:46:29 +0900

    prepare test.

---
+++ tests/ClientTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Uenoryo\Awsps\Test;
+
+require './vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use Uenoryo\Awsps\Client;
+
+class ClientTest extends TestCase
+{
+    public function testNew()
+    {
+        //
+    }
+}
