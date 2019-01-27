8e02d0d42f449cadc75e934bd1c9839450496661
uenoryo <r.r.ag0611@outlook.jp>
2019-01-27 14:33:33 +0900

    Add testNew.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -11,26 +11,39 @@ class ClientTest extends TestCase
     public function testNew()
     {
         $tests = [
-        	'title' => 'success',
-        	'input' => function() {
-        		$cnf = Config::new();
-        		$cnf->region     = 'test-region';
-        		$cnf->version    = '1.0.0';
-        		$cnf->path       = '/Test';
-        		$cnf->exportType = 'json';
-        		return $cnf;
-        	},
-        	'expect' => [
-        		'region'     => 'test-region',
-        		'version'    => '1.0.0',
-        		'path'       => '/Test',
-        		'exportType' => 'json',
+        	[
+	        	'title' => 'success',
+	        	'input' => function() {
+	        		$cnf = Config::new();
+	        		$cnf->region     = 'test-region';
+	        		$cnf->path       = '/Test';
+	        		$cnf->exportType = 'json';
+	        		return $cnf;
+	        	},
+	        	'expect' => [
+	        		'region' => 'test-region',
+	        		'path'   => '/Test',
+	        	],
+        	],
+        	[
+	        	'title' => 'success default config',
+	        	'input' => function() {
+	        		return Config::new();
+	        	},
+	        	'expect' => [
+	        		'region' => 'ap-northeast-1',
+	        		'path'   => '/',
+	        	],
         	],
         ];
 
         foreach ($tests as $t) {
         	$client = Client::new($t['input']());
-        	$this->assetSame($client->path, $t['expect']['path'], $t['title']);
+        	$this->assertSame($client->ssmClient->getRegion(), $t['expect']['region'], $t['title']);
+
+        	$this->assertSame($client->path, $t['expect']['path'], $t['title']);
+
+        	$this->assertNotNull($client->exporter, $t['title']);
         }
     }
 }
