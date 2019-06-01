92dcbd4c7fe68b60ff0d3de45459bf8c71a5f2f1
uenoryo <r.r.ag0611@outlook.jp>
2019-06-01 18:07:17 +0900

    noEscapeでjson_encodeできるようにする.

--- src/Exporter/Json.php
+++ src/Exporter/Json.php
@@ -6,12 +6,22 @@ use Uenoryo\Awsps\Exporter;
 
 class Json implements Exporter
 {
+	public $noEscape = false;
+
 	public function export($data)
 	{
 		$result = [];
 		foreach ($data as $r) {
 			$result[$r->name] = $r->value;
 		}
-		return json_encode($result);
+
+		$options = [];
+		if ($this->noEscape) {
+			$options = [
+				JSON_UNESCAPED_SLASHES,
+				JSON_UNESCAPED_UNICODE,
+			];
+		}
+		return json_encode($result, $options);
 	}
 }
